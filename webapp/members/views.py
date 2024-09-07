3c2a754387832c4aca8fd5971af31233676c0044
vinyas-bharadwaj <vinyasbharadwaj101@gmail.com>
2024-09-08 01:50:46 +0530

    added user authentication

---
+++ webapp/members/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout
+from django.urls import reverse_lazy
+
+# Create your views here.
+class RegistrationForm(generic.CreateView):
+  form_class = UserCreationForm
+  template_name = 'registration/register.html'
+  success_url = reverse_lazy('login')
+
--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # My apps
     'app',
+    'members',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
