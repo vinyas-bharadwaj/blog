23f314639c4b112d2377a17d5faf0cf7c5989952
vinyas-bharadwaj <vinyasbharadwaj101@gmail.com>
2024-09-13 22:42:08 +0530

    improved the UI and added a user profile section

--- webapp/members/urls.py
+++ webapp/members/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
   path('profile/', views.UserEditForm.as_view(), name='profile'),
   #path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html')),
   path('password/', views.PasswordChangeView.as_view()),
-  path('password/change/success', views.password_change_success, name='password-change-success')
+  path('password/change/success', views.password_change_success, name='password-change-success'),
+  path('<int:pk>/profile', views.ProfilePageView.as_view(), name='user-profile'),
 ]
--- webapp/members/views.py
+++ webapp/members/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views import generic
 from django.contrib.auth import logout
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileForm, ChangePassword
+from app.models import Profile, Post
+
 
 # Create your views here.
 class RegistrationForm(generic.CreateView):
@@ -18,6 +20,21 @@ class UserEditForm(generic.UpdateView):
 
   def get_object(self):
     return self.request.user
+  
+class ProfilePageView(generic.DetailView):
+  model = Profile
+  template_name = 'registration/user-profile.html'
+
+  def get_context_data(self, *args, **kwargs):
+    context = super(ProfilePageView, self).get_context_data(*args, **kwargs)
+
+    page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+    user_posts = Post.objects.all().filter(author=page_user.user.id)
+
+    context['page_user'] = page_user
+    context['user_posts'] = user_posts
+    return context
+
   
 class PasswordChangeView(PasswordChangeView):
   form_class = ChangePassword
